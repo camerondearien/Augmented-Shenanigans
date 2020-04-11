fb206621f0642a915a45cf0d22d2d877656658c0
AdamMOdell <AdamMOdell@users.noreply.github.com>
2020-04-11 15:18:44 -0700

    Added hyperlink script

---
+++ Augmented Shenanigans/Assets/Common/Scripts/hyperlink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class hyperlink : MonoBehaviour
+{
+
+    public string link;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hyperlink script loaded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Application.OpenURL(link);
+    }
+}
